f2f32b288e613959889545f74a3f7f05307e8491
minsun <judy.leems@gmail.com>
2021-12-05 21:55:20 +0900

    custom hook 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState, useEffect } from "react";
+
+function useLocalStorage(itemName, value = "") {
+  const [state, setState] = useState(() => {
+    console.log("initialize");
+    return window.localStorage.getItem(itemName) || "";
+  });
+
+  useEffect(() => {
+    console.log(itemName);
+    window.localStorage.setItem(itemName, state);
+  }, [state]);
+
+  return [state, setState];
+}
 
 function App() {
+  const [keyword, setKeyword] = useLocalStorage("keyword");
+  const [result, setResult] = useLocalStorage("result");
+  const [typing, setTyping] = useLocalStorage("typing", false);
+
+  console.log("render");
+
+  function handleChange(event) {
+    setKeyword(event.target.value);
+    setTyping(true);
+  }
+
+  function handleClick() {
+    setTyping(false);
+    setResult(`We find results of ${keyword}`);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={handleChange} value={keyword} />
+      <button onClick={handleClick}>search</button>
+      <p>{typing ? `Looking for ${keyword}...` : result}</p>
     </div>
   );
 }
